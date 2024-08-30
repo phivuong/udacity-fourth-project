646dcc09b64971a8edda826ee699a5c8359cb0af
phivuong <52486346+phivuong@users.noreply.github.com>
2024-08-30 15:58:42 +0700

    Add InstrumentationKey

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -23,7 +23,7 @@ from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
-InstrumentationKey = 'InstrumentationKey=5a52b4df-11a4-45b6-a908-2e276b948f82'
+InstrumentationKey = 'InstrumentationKey=5233015b-2f74-4fe8-8f17-833b49fa29ae'
 
 config_integration.trace_integrations(['logging'])
 config_integration.trace_integrations(['requests'])
@@ -141,6 +141,6 @@ def index():
 
 if __name__ == "__main__":
     # TODO: Use the statement below when running locally
-    # app.run() 
+    app.run() 
     # TODO: Use the statement below before deployment to VMSS
-    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+    # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
